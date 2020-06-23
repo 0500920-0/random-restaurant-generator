3c83716920d88dc73813e829705140f427c99233
0500920-0 <tomfongmi@gmail.com>
2020-06-23 13:35:56 +0800

    Fix compatibility
    Drop private class field

--- js/RandomizerGenerator.js
+++ js/RandomizerGenerator.js
@@ -21,26 +21,26 @@
 
 /** Make a new Random Generator with weights 1. */
 export default class RandomizerGenerator {
-  /** @type {RandomItem[]} */
-  #initialList = [];
-
-  /** @type {RandomItem[]} */
-  #randomList = [];
-
-  /** @type {RandomItem[]} */
-  #pickedList = [];
-
-  // /** @type {string} */
-  // #weightName;
-
-  /** @type {number} */
-  #totalWeight = 0;
-
   /** 
    *
    * @param {RandomItem[]} initialList
    */
   constructor(initialList) {
+
+    /** @type {RandomItem[]} */
+    this._initialList = [];
+
+    /** @type {RandomItem[]} */
+    this._randomList = [];
+
+    /** @type {RandomItem[]} */
+    this._pickedList = [];
+
+    // /** @type {string} */
+    // this._weightName;
+
+    /** @type {number} */
+    this._totalWeight = 0;
     this.reset(initialList);
   }
 
@@ -48,18 +48,18 @@ export default class RandomizerGenerator {
    * Using a new list for the Random Generator.
    * @param {RandomItem[]=} newList
    */
-  reset(newList = this.#initialList) {
-    this.#totalWeight = 0;
+  reset(newList = this._initialList) {
+    this._totalWeight = 0;
     for (let item of newList) {
       // if property 'weight' does not exist
       if (item.weight == null) {
         item.weight = item.defaultWeight;
       }
-      this.#totalWeight += item.weight;
+      this._totalWeight += item.weight;
     }
-    this.#initialList = [...newList];
-    this.#randomList = [...newList];
-    this.#pickedList = [];
+    this._initialList = [...newList];
+    this._randomList = [...newList];
+    this._pickedList = [];
 
   }
 
@@ -68,20 +68,20 @@ export default class RandomizerGenerator {
    * @returns {RandomItem}
    */
   pick() {
-    if (this.#randomList.length === 0) throw new RangeError('↙請重置生成器');
-    const totalWeight = this.#totalWeight;
+    if (this._randomList.length === 0) throw new RangeError('↙請重置生成器');
+    const totalWeight = this._totalWeight;
     let randomNumber = Math.random() * totalWeight;
     let pickedItem;
-    for (let item of this.#randomList) {
+    for (let item of this._randomList) {
       randomNumber -= item.weight;
       if (randomNumber < 0) {
         pickedItem = item;
         break;
       }
     }
-    this.#randomList.splice(this.#randomList.findIndex(findItem => findItem === pickedItem), 1);
-    this.#pickedList.push(pickedItem);
-    this.#totalWeight -= pickedItem.weight;
+    this._randomList.splice(this._randomList.findIndex(findItem => findItem === pickedItem), 1);
+    this._pickedList.push(pickedItem);
+    this._totalWeight -= pickedItem.weight;
     return pickedItem;
   }
 
@@ -89,11 +89,11 @@ export default class RandomizerGenerator {
    * @returns {RandomProbabilityItem[]}
    */
   getProbabilityList() {
-    return this.#randomList.map((item) => ({
+    return this._randomList.map((item) => ({
       name: item.name,
       alias: item.alias ?? undefined,
       weight: item.weight,
-      probability: item.weight / this.#totalWeight,
+      probability: item.weight / this._totalWeight,
     }));
   }
 
